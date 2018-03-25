4e6f758e9be00b55fa547d9595cc1561e506eea4
hamdigaied <33940594+hamdigaied@users.noreply.github.com>
2018-03-25 14:03:32 +0100

    Add files via upload

---
+++ PercentageApp/src/percentageapp/PercentageApp.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package percentageapp;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Asus
+ */
+public class PercentageApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        body b = new body();
+        b.show();
+    }
+    
+}
